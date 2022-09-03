0922c3763a34c9842ce9c4ae4f37fca8c4ea249d
KueenKat <Mignonne30@Hotmail.com>
2022-09-03 15:47:02 -0500

    test

---
+++ src/com/upskill/java/HelloWorld.java
@@ -0,0 +1,24 @@
+package com.upskill.java;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World, This is Kathy");
+		System.out.println("We are Wolverine");
+		
+		int a = 5;
+		int b = 8;
+		int c = a+b;
+				System.out.println(c);
+				
+				String city = "New York";
+				String state = "NY";
+				System.out.println(city);
+				System.out.println(state);
+				
+				boolean isGood = true;
+				System.out.println(isGood);
+}
+}
+		
